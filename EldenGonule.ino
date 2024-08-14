5be2706db6e0e70ecd2dd3b8840e3d3d8af56886
Yunus Köse <38354843+koseeYunus@users.noreply.github.com>
2024-08-14 15:23:23 +0300

    functions added

--- EldenGonule.ino
+++ EldenGonule.ino
@@ -13,6 +13,7 @@ SoftwareSerial mySerial_git_mp3(10, 11); // RX, TX
 
 int dil=1;
 
+int[] sesler = [1,2,3]
 
 
 void setup () {
@@ -40,17 +41,13 @@ bool sol1, sol2, sol3, sol4, sol5, sol6, sol7, sol8, sol9, sol10, sol11, sol12 =
 
 int tekrar=0;
 
-void loop () {
-  sag1, sag2, sag3, sag4, sag5, sag6, sag7, sag8, sag9, sag10, sag11, sag12 = 1;
-  sol1, sol2, sol3, sol4, sol5, sol6, sol7, sol8, sol9, sol10, sol11, sol12 = 1;
-
+void dilDegisim(){
   //Dili Turkce Yapma
   if (digitalRead(2) == LOW) {    
     dil=1;
     mp3_play (501);
     lcdYaz("Dil","Turkce","Yapildi "," ");
     Serial.println("Dil Turkce Yapildi");
-    delay(1500);
   }
   //Dili İngilizce Yapma
   if (digitalRead(3) == LOW) {    
@@ -58,7 +55,6 @@ void loop () {
     mp3_play (502);
     lcdYaz("The Language","Was Made in","English "," ");
     Serial.println("Dil Ingilizce Yapildi");
-    delay(1500);
   }
   //Dili İspanyolca Yapma
   if (digitalRead(4) == LOW) {    
@@ -66,18 +62,13 @@ void loop () {
     mp3_play (503);
     lcdYaz("El Idioma","Se Hizo en","Español "," ");
     Serial.println("Dil İsponyalca Yapildi");
-    delay(1500);
   }
+  delay(1500);
+}
 
-  /*//BOZKURT MERİH DEMİRAL
-  if ((digitalRead(25) == 0) ) {    
-      mp3_play (1071);
-      lcdYaz("      BOZKURT","       MELIH","     DEMIRAL","      --..-- ");
-  }
-  if ((digitalRead(28) == 0) ) {    
-      mp3_play (1071);
-      lcdYaz("      BOZKURT","       MELIH","     DEMIRAL","      --..-- ");
-  }*/
+void dokunmatikSensorOku(){
+  sag1, sag2, sag3, sag4, sag5, sag6, sag7, sag8, sag9, sag10, sag11, sag12 = 1;
+  sol1, sol2, sol3, sol4, sol5, sol6, sol7, sol8, sol9, sol10, sol11, sol12 = 1;
 
   for(tekrar=0; tekrar<5000 ; tekrar++){
     sag1 = digitalRead(22);
@@ -106,159 +97,19 @@ void loop () {
     sol11 = digitalRead(44);
     sol12 = digitalRead(45);
   }
+}
+
+void loop () {
+  
+  dilDegisim();
+
+  dokunmatikSensorOku();
+
 
   // 1 ise dil Türkce
   if(dil==1){
-    //Birinci El
-    if (sag1 == 0 && sol1==0) {    
-      mp3_play (28);
-      lcdYaz("Bir","dokunus","Bir "," ");
-    }
-    else if (sag1 == 0 && sol2==0) {    
-      mp3_play (29);
-      lcdYaz("Iki","dokunus","Iki "," ");
-    }
-    else if (sag1 == 0 && sol3==0) {    
-      mp3_play (30);
-      lcdYaz("Uc","dokunus","Uc "," ");
-    }
 
-    else if (sag1 == 0) {    
-      mp3_play (1);
-      lcdYaz("Istiyorum",""," "," ");
-    }
-
-    else if (sag2 == 0) {    
-      mp3_play (2);
-      lcdYaz("Evet","",""," ");
-    }
-
-    else if (sag3 == 0) {    
-      mp3_play (3);
-      lcdYaz("Hayir","",""," ");
-    }
-
-    else if (sag4 == 0) {    
-      mp3_play (4);
-      lcdYaz("Selamun","Aleykum",""," ");
-    }
-
-    else if (sag5 == 0) {    
-      mp3_play (5);
-      lcdYaz("Nasilsin","",""," ");
-    }
-
-    else if (sag6 == 0) {    
-      mp3_play (6);
-      lcdYaz("Ne","Yapiyorsun",""," ");
-    }
-
-    else if (sag7 == 0) 
-    {    
-      mp3_play (7);
-      lcdYaz("Nereye","Gidiyorsun","?"," ");
-    }
-
-    else if (sag8 == 0) 
-    {    
-      mp3_play (8);
-      lcdYaz("Bir","Dakika",""," ");
-    }
-
-    else if (sag9 == 0) 
-    {    
-      mp3_play (9);
-      lcdYaz("Ne ","Zaman","?"," ");
-    }
-
-    else if (sag10 == 0) 
-    {    
-      mp3_play (10);
-      lcdYaz("Allah'a","Emanet"," "," ");
-    }
-
-    else if (sag11 == 0) 
-    {    
-      mp3_play (11);
-      lcdYaz("Kac","",""," ");
-    }
-
-    else if (sag12 == 0) 
-    {    
-      mp3_play (12);
-      lcdYaz("Gel","",""," ");
-    }
-
-    //İkinci El
-    else if (sol1 == 0) {    
-      mp3_play (13);
-      lcdYaz("Git","",""," ");
-    }
-
-    else if (sol2 == 0) {    
-      mp3_play (14);
-      lcdYaz("Ozur","Dilerim"," "," ");
-    }
-
-    else if (sol3 == 0) {    
-      mp3_play (15);
-      lcdYaz("Tesekkur","Ederim",""," ");
-    }
-
-    else if (sol4 == 0) {    
-      mp3_play (16);
-      lcdYaz("Insallah",""," "," ");
-    }
-
-    else if (sol5 == 0) {    
-      mp3_play (17);
-      lcdYaz("Memleket",""," "," ");
-    }
-
-    else if (sol6 == 0) 
-    {    
-      mp3_play (18);
-      lcdYaz("Kolay","","  ","");
-    }
-
-    else if (sol7 == 0) 
-    {    
-      mp3_play (1071);
-      lcdYaz("Zor","","","");
-    }
-
-    else if (sol8 == 0) 
-    {    
-      mp3_play (20);
-      lcdYaz("Var","","","");
-    }
-
-    else if (sol9 == 0) 
-    {    
-      mp3_play (21);
-      lcdYaz("Yok","",""," ");
-    }
-
-    else if (sol10 == 0) 
-    {    
-      mp3_play (22);
-      lcdYaz("Bitti ",""," "," ");
-    }
-
-    else if (sol11 == 0) 
-    {    
-      mp3_play (23);
-      lcdYaz("Basla","","","");
-    }
-
-    else if (sol12 == 0) 
-    {    
-      mp3_play (24);
-      lcdYaz("Tekrar","",""," ");
-    }
-    else {
-      Serial.println("Turkce Seslendirme Taninamadı ??? ");
-    }
+    
 
   }
 
@@ -420,7 +271,7 @@ void loop () {
 
     else if (sag2 == 0) {    
       mp3_play (202);
-      lcdYaz("Si","","?","");
+      lcdYaz("Si","","","");
     }
 
     else if (sag3 == 0) {    
@@ -556,7 +407,7 @@ void loop () {
 
   }
   else{
-    Serial.println("Dil degiskeni tanımalanamadı !!! ");
+    Serial.println("Dil degiskeni tanımlanamadı !!! ");
   }
 
 }
@@ -589,4 +440,409 @@ void kisaMetod(){
   }
 }
 
+string turkceCumleler[1000][4]={
+  {"Merhaba" , " " , " " , " "},    // 1
+  {"Nasilsin" , " " , " " , " "},   // 2
+  {"Bugun" , "Ne " , "Yapacaksin " , "? "},   // 3
+  {"Ise" , "Gidecegim " , " " , " "},   // 4
+}
 
+void TurkceKonus(string Cumleler[1000][4] , int Ses){
+  if (sag1 == 0 && sol1==0) {    
+      mp3_play (ses + 0);
+      lcdYaz(Cumleler[0][0], Cumleler[0][1], Cumleler[0][2], Cumleler[0][3],);
+    }
+    else if (sag1 == 0 && sol2==0) {    
+      mp3_play (ses + 1);
+      lcdYaz(Cumleler[1][0], Cumleler[1][1], Cumleler[1][2], Cumleler[1][3],);
+    }
+    else if (sag1 == 0 && sol3==0) {    
+      mp3_play (ses + 2);
+      lcdYaz(Cumleler[2][0], Cumleler[2][1], Cumleler[2][2], Cumleler[2][3],);
+    }
+    else if (sag1 == 0 && sol4==0) {    
+      mp3_play (ses + 3);
+      lcdYaz(Cumleler[3][0], Cumleler[3][1], Cumleler[3][2], Cumleler[3][3],);
+    }    
+    else if (sag1 == 0 && sol5==0) {    
+      mp3_play (ses + 4);
+      lcdYaz(Cumleler[4][0], Cumleler[4][1], Cumleler[4][2], Cumleler[4][3],);
+    }
+    int kombinasyon=0;
+    for(int sag=0 ; sag<12 ; sag++){
+      for(int sol=0 ; sol<12 ; sol++){
+        if (sagDok[sag] == 0 && solDok[sol]==0) {    
+          mp3_play (ses + kombinasyon);
+          lcdYaz(Cumleler[kombinasyon][0], Cumleler[kombinasyon][1], Cumleler[kombinasyon][2], Cumleler[kombinasyon][3],);
+          break;
+        }
+        kombinasyon+=1;
+      }
+    }
+
+    else if (sag1 == 0 && sol6==0) {    
+      mp3_play (30);
+      lcdYaz("altı","dokunus","altı "," ");
+    } 
+    else if (sag1 == 0 && sol7==0) {    
+      mp3_play (31);
+      lcdYaz("yedi","dokunus","yedi "," ");
+    }
+    else if (sag1 == 0 && sol8==0) {    
+      mp3_play (32);
+      lcdYaz("sekiz","dokunus","sekiz "," ");
+    }
+    else if (sag1 == 0 && sol9==0) {    
+      mp3_play (33);
+      lcdYaz("dokuz","dokunus","dokuz "," ");
+    }
+    else if (sag1 == 0 && sol10==0) {    
+      mp3_play (34);
+      lcdYaz("on","dokunus","on "," ");
+    }
+    else if (sag1 == 0 && sol11==0) {    
+      mp3_play (35);
+      lcdYaz("onbir","dokunus","onbir "," ");
+    }
+    else if (sag1 == 0 && sol12==0) {    
+      mp3_play (36);
+      lcdYaz("oniki","dokunus","oniki "," ");
+    }
+
+    else if (sag2 == 0 && sol1==0) {    
+      mp3_play (25);
+      lcdYaz("Bir","2dokunus","Bir "," ");
+    }
+    else if (sag2 == 0 && sol2==0) {    
+      mp3_play (26);
+      lcdYaz("Iki","2dokunus","Iki "," ");
+    }
+    else if (sag2 == 0 && sol3==0) {    
+      mp3_play (27);
+      lcdYaz("Uc","2dokunus","Uc "," ");
+    }
+    else if (sag2 == 0 && sol4==0) {    
+      mp3_play (28);
+      lcdYaz("dort","2dokunus","dort "," ");
+    }    
+    else if (sag2 == 0 && sol5==0) {    
+      mp3_play (29);
+      lcdYaz("bes","2dokunus","bes "," ");
+    }
+    else if (sag2 == 0 && sol6==0) {    
+      mp3_play (30);
+      lcdYaz("altı","2dokunus","altı "," ");
+    } 
+    else if (sag2 == 0 && sol7==0) {    
+      mp3_play (31);
+      lcdYaz("yedi","2dokunus","yedi "," ");
+    }
+    else if (sag2 == 0 && sol8==0) {    
+      mp3_play (32);
+      lcdYaz("sekiz","2dokunus","sekiz "," ");
+    }
+    else if (sag2 == 0 && sol9==0) {    
+      mp3_play (33);
+      lcdYaz("dokuz","2dokunus","dokuz "," ");
+    }
+    else if (sag2 == 0 && sol10==0) {    
+      mp3_play (34);
+      lcdYaz("on","2dokunus","on "," ");
+    }
+    else if (sag2 == 0 && sol11==0) {    
+      mp3_play (35);
+      lcdYaz("onbir","2dokunus","onbir "," ");
+    }
+    else if (sag2 == 0 && sol12==0) {    
+      mp3_play (36);
+      lcdYaz("oniki","2dokunus","oniki "," ");
+    }
+
+    else if (sag3 == 0 && sol1==0) {    
+      mp3_play (25);
+      lcdYaz("Bir","3dokunus","Bir "," ");
+    }
+    else if (sag3 == 0 && sol2==0) {    
+      mp3_play (26);
+      lcdYaz("Iki","3dokunus","Iki "," ");
+    }
+    else if (sag3 == 0 && sol3==0) {    
+      mp3_play (27);
+      lcdYaz("Uc","3dokunus","Uc "," ");
+    }
+    else if (sag3 == 0 && sol4==0) {    
+      mp3_play (28);
+      lcdYaz("dort","3dokunus","dort "," ");
+    }    
+    else if (sag3 == 0 && sol5==0) {    
+      mp3_play (29);
+      lcdYaz("bes","3dokunus","bes "," ");
+    }
+    else if (sag3 == 0 && sol6==0) {    
+      mp3_play (30);
+      lcdYaz("altı","3dokunus","altı "," ");
+    } 
+    else if (sag3 == 0 && sol7==0) {    
+      mp3_play (31);
+      lcdYaz("yedi","3dokunus","yedi "," ");
+    }
+    else if (sag3 == 0 && sol8==0) {    
+      mp3_play (32);
+      lcdYaz("sekiz","3dokunus","sekiz "," ");
+    }
+    else if (sag3 == 0 && sol9==0) {    
+      mp3_play (33);
+      lcdYaz("dokuz","3dokunus","dokuz "," ");
+    }
+    else if (sag3 == 0 && sol10==0) {    
+      mp3_play (34);
+      lcdYaz("on","3dokunus","on "," ");
+    }
+    else if (sag3 == 0 && sol11==0) {    
+      mp3_play (35);
+      lcdYaz("onbir","3dokunus","onbir "," ");
+    }
+    else if (sag3 == 0 && sol12==0) {    
+      mp3_play (36);
+      lcdYaz("oniki","3dokunus","oniki "," ");
+    }
+
+
+    else  if (sag4 == 0 && sol1==0) {    
+      mp3_play (25);
+      lcdYaz("Bir","4dokunus","Bir "," ");
+    }
+    else if (sag4 == 0 && sol2==0) {    
+      mp3_play (26);
+      lcdYaz("Iki","4dokunus","Iki "," ");
+    }
+    else if (sag4 == 0 && sol3==0) {    
+      mp3_play (27);
+      lcdYaz("Uc","4dokunus","Uc "," ");
+    }
+    else if (sag4 == 0 && sol4==0) {    
+      mp3_play (28);
+      lcdYaz("dort","4dokunus","dort "," ");
+    }    
+    else if (sag4 == 0 && sol5==0) {    
+      mp3_play (29);
+      lcdYaz("bes","4dokunus","bes "," ");
+    }
+    else if (sag4 == 0 && sol6==0) {    
+      mp3_play (30);
+      lcdYaz("altı","4dokunus","altı "," ");
+    } 
+    else if (sag4 == 0 && sol7==0) {    
+      mp3_play (31);
+      lcdYaz("yedi","4dokunus","yedi "," ");
+    }
+    else if (sag4 == 0 && sol8==0) {    
+      mp3_play (32);
+      lcdYaz("sekiz","4dokunus","sekiz "," ");
+    }
+    else if (sag4 == 0 && sol9==0) {    
+      mp3_play (33);
+      lcdYaz("dokuz","4dokunus","dokuz "," ");
+    }
+    else if (sag4 == 0 && sol10==0) {    
+      mp3_play (34);
+      lcdYaz("on","4dokunus","on "," ");
+    }
+    else if (sag4 == 0 && sol11==0) {    
+      mp3_play (35);
+      lcdYaz("onbir","4dokunus","onbir "," ");
+    }
+    else if (sag4 == 0 && sol12==0) {    
+      mp3_play (36);
+      lcdYaz("oniki","4dokunus","oniki "," ");
+    }
+
+
+    else if (sag5 == 0 && sol1==0) {    
+      mp3_play (25);
+      lcdYaz("Bir","5dokunus","Bir "," ");
+    }
+    else if (sag5 == 0 && sol2==0) {    
+      mp3_play (26);
+      lcdYaz("Iki","5dokunus","Iki "," ");
+    }
+    else if (sag5 == 0 && sol3==0) {    
+      mp3_play (27);
+      lcdYaz("Uc","5dokunus","Uc "," ");
+    }
+    else if (sag5 == 0 && sol4==0) {    
+      mp3_play (28);
+      lcdYaz("dort","5dokunus","dort "," ");
+    }    
+    else if (sag5 == 0 && sol5==0) {    
+      mp3_play (29);
+      lcdYaz("bes","5dokunus","bes "," ");
+    }
+    else if (sag5 == 0 && sol6==0) {    
+      mp3_play (30);
+      lcdYaz("altı","5dokunus","altı "," ");
+    } 
+    else if (sag5 == 0 && sol7==0) {    
+      mp3_play (31);
+      lcdYaz("yedi","5dokunus","yedi "," ");
+    }
+    else if (sag5 == 0 && sol8==0) {    
+      mp3_play (32);
+      lcdYaz("sekiz","5dokunus","sekiz "," ");
+    }
+    else if (sag5 == 0 && sol9==0) {    
+      mp3_play (33);
+      lcdYaz("dokuz","5dokunus","dokuz "," ");
+    }
+    else if (sag5 == 0 && sol10==0) {    
+      mp3_play (34);
+      lcdYaz("on","5dokunus","on "," ");
+    }
+    else if (sag5 == 0 && sol11==0) {    
+      mp3_play (35);
+      lcdYaz("onbir","5dokunus","onbir "," ");
+    }
+    else if (sag5 == 0 && sol12==0) {    
+      mp3_play (36);
+      lcdYaz("oniki","5dokunus","oniki "," ");
+    }
+
+    // ...
+
+    else if (sag1 == 0) {    
+      mp3_play (144);
+      lcdYaz("Istiyorum",""," "," ");
+    }
+
+    else if (sag2 == 0) {    
+      mp3_play (145);
+      lcdYaz("Evet","",""," ");
+    }
+
+    else if (sag3 == 0) {    
+      mp3_play (146);
+      lcdYaz("Hayir","",""," ");
+    }
+
+    else if (sag4 == 0) {    
+      mp3_play (147);
+      lcdYaz("Selamun","Aleykum",""," ");
+    }
+
+    else if (sag5 == 0) {    
+      mp3_play (148);
+      lcdYaz("Nasilsin","",""," ");
+    }
+
+    else if (sag6 == 0) {    
+      mp3_play (149);
+      lcdYaz("Ne","Yapiyorsun",""," ");
+    }
+
+    else if (sag7 == 0) 
+    {    
+      mp3_play (150);
+      lcdYaz("Nereye","Gidiyorsun","?"," ");
+    }
+
+    else if (sag8 == 0) 
+    {    
+      mp3_play (151);
+      lcdYaz("Bir","Dakika",""," ");
+    }
+
+    else if (sag9 == 0) 
+    {    
+      mp3_play (152);
+      lcdYaz("Ne ","Zaman","?"," ");
+    }
+
+    else if (sag10 == 0) 
+    {    
+      mp3_play (153);
+      lcdYaz("Allah'a","Emanet"," "," ");
+    }
+
+    else if (sag11 == 0) 
+    {    
+      mp3_play (154);
+      lcdYaz("Kac","",""," ");
+    }
+
+    else if (sag12 == 0) 
+    {    
+      mp3_play (155);
+      lcdYaz("Gel","",""," ");
+    }
+
+    //İkinci El
+    else if (sol1 == 0) {    
+      mp3_play (156);
+      lcdYaz("Git","",""," ");
+    }
+
+    else if (sol2 == 0) {    
+      mp3_play (158);
+      lcdYaz("Ozur","Dilerim"," "," ");
+    }
+
+    else if (sol3 == 0) {    
+      mp3_play (159);
+      lcdYaz("Tesekkur","Ederim",""," ");
+    }
+
+    else if (sol4 == 0) {    
+      mp3_play (160);
+      lcdYaz("Insallah",""," "," ");
+    }
+
+    else if (sol5 == 0) {    
+      mp3_play (161);
+      lcdYaz("Memleket",""," "," ");
+    }
+
+    else if (sol6 == 0) 
+    {    
+      mp3_play (162);
+      lcdYaz("Kolay","","  ","");
+    }
+
+    else if (sol7 == 0) 
+    {    
+      mp3_play (163);
+      lcdYaz("Zor","","","");
+    }
+
+    else if (sol8 == 0) 
+    {    
+      mp3_play (164);
+      lcdYaz("Var","","","");
+    }
+
+    else if (sol9 == 0) 
+    {    
+      mp3_play (165);
+      lcdYaz("Yok","",""," ");
+    }
+
+    else if (sol10 == 0) 
+    {    
+      mp3_play (166);
+      lcdYaz("Bitti ",""," "," ");
+    }
+
+    else if (sol11 == 0) 
+    {    
+      mp3_play (167);
+      lcdYaz("Basla","","","");
+    }
+
+    else if (sol12 == 0) 
+    {    
+      mp3_play (168);
+      lcdYaz("Tekrar","",""," ");
+    }
+    else {
+      Serial.println("Turkce Seslendirme Taninamadı ??? ");
+    }
+}
